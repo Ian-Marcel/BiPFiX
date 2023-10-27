f95555dc4f3f99acd2b5c94bd73206957e50fd5d
MatheusGoncalvesM <matheusmachado2005@hotmail.com>
2023-10-27 18:14:38 -0300

    Adicionado Mercado e´detalhes da conta

--- Conta/index.php
+++ Conta/index.php
@@ -3,14 +3,64 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="shortcut icon" href="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbrandslogos.com%2Fwp-content%2Fuploads%2Fimages%2Flarge%2Fphp-logo.png&f=1&nofb=1&ipt=41acf791031a9c7f7571982d2ab60ad04951397125ce67492e5feb31bc878e13&ipo=images" type="image/x-icon">
         <title>BiPFiX - Conta</title>
     </head>
     <body>
+    <div class="tudo">
+        <div class="gradient">
+    <div class="largura">Bem vindo de volta Display Name!</div>
+    <div class="caixazona">
+    <div style="border: 0px; background-color: #424A53;" class="menu-lateral">
+        <ul>
+            <li><a href="#" class="menu-item"><img style="border-radius: 15px; " src="../Ícones/Mais.svg"><span style="font-family: montserrat; font-size: 15px; padding-top: 10px;">Home</span></a></li>
+            <li><a href="#" class="menu-item"><img style="border-radius: 15px; " src="../Ícones/Suas Ordens.svg"><span style="font-family: montserrat; font-size: 15px; padding-top: 10px;">Perfil</span></a></li>
+            <li><a href="#" class="menu-item"><img style="border-radius: 15px; " src="../Ícones/Criar Ordem.svg"><span style="font-family: montserrat; font-size: 15px; padding-top: 10px;">Ordens</span></a></li>
+            <li><a href="#" class="menu-item"><img style="border-radius: 15px; " src="../Ícones/Mercado.svg"><span style="font-family: montserrat; font-size: 15px; padding-top: 10px;">ABC</span></a></li>
+            <li><a href="#" class="menu-item"><img style="border-radius: 15px; " src="../Ícones/Conta.svg"><span style="font-family: montserrat; font-size: 15px; padding-top: 10px;">outros</span></a></li>
+        </ul>
+    </div>
+    <div class="caixazonadireita">
+        <div class="detalhescontainer"><!--Começa o código da parte de detalhes da conta e suas ordens-->
+            <div style="background-color: rgb(59, 59, 59);" class="setor">
+                <div style="font-size: 40px;" class="texto">Detalhes da conta</div>
+                <div class="texto" style="font-size: 20px; margin-top: 20px;">ID:</div>
+                <label for="id"></label>
+                <input type="text" name="id"  required>
+                <label for="nomepublico"><b style="font-family: montserrat; padding-top: 50px;">Nome Público:</b></label>
+                <input type="text" name="nomepublico" required>
+                <div class="texto" style="font-size: 20px;">Senha:</div>
+                <label for="senha"></label>
+                <input type="password" name="senha"  required>
+            </div>
+        </div><!--Termina o código da parte de detalhes da conta e suas ordens-->
+        <div class="ordens">
+            <div style="font-size: 40px;" class="texto">Suas ordens</div>
+            <img style="height: 200px; width: 200px;" src="https://cdn-icons-png.flaticon.com/512/7801/7801791.png">
+            <h3 style="font-family: montserrat; font-size: 20px;">Você não tem nenhum tipo de ordem ativa no momento,<br>
+                acesse o mercado de ordens para
+                comprar ou vender.</h3>
+        </div>
+</div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+    
+setInterval(updateGradient,10);
+    </script>
+<script>
+    const menuItems = document.querySelectorAll('.menu-item');
 
-        <?php
-        echo '<h1>Hello PHP!</h1>';
-        ?>
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
 
-    </body>
+        menuItems.forEach(i => i.classList.remove('active'));
+
+        item.classList.add('active');
+    });
+});
+const dropdownButton = document.querySelector('.dropdown button');
+const dropdownItems = document.querySelectorAll('.dropdown-item');
+</script>
+</body>
 </html>
--- Mercado/index.php
+++ Mercado/index.php
@@ -1,16 +1,141 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="shortcut icon" href="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbrandslogos.com%2Fwp-content%2Fuploads%2Fimages%2Flarge%2Fphp-logo.png&f=1&nofb=1&ipt=41acf791031a9c7f7571982d2ab60ad04951397125ce67492e5feb31bc878e13&ipo=images" type="image/x-icon">
-        <title>BiPFiX - Mercado</title>
-    </head>
-    <body>
-
-        <?php
-        echo '<h1>Hello PHP!</h1>';
-        ?>
-
-    </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Mercado</title>
+</head>
+<style>
+    .button {
+      background-color: #34C848;
+      border: none;
+      color: white;
+      padding: 10px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      border: 2px solid black;
+      font-size: 16px;
+      margin: 4px 2px;
+      cursor: pointer;
+      transition: 0.3 ease;
+      cursor: pointer;
+    }
+    .button:hover{
+        transform: scale(1.1);
+        transition: .3s ease;
+        cursor: pointer;
+    }
+    .h3coluna{
+        border-bottom: 2px solid rgb(249, 249, 249);
+        padding: 10px;
+        border-left: 2px solid rgb(249, 249, 249);
+        font-family: Montserrat;
+        font-size: 15px;
+    }
+    .colunalabel{
+        padding:10px;
+        font-family: montserrat;
+        border-radius: 15px; 
+        padding: 10px; 
+        background-color: rgba(30, 30, 30);
+        font-size: 15px;
+    }
+    </style>
+<body>
+    <div class="caixaprincipal">
+    <div style="border: 0px; background-color: #424A53;" class="menu-lateral">
+        <ul>
+            <li><a href="#" class="menu-item"><img style="border-radius: 15px; " src="../Ícones/Mais.svg"><span style="font-family: montserrat; font-size: 15px; padding-top: 10px;">Home</span></a></li>
+            <li><a href="#" class="menu-item"><img style="border-radius: 15px; " src="../Ícones/Suas Ordens.svg"><span style="font-family: montserrat; font-size: 15px; padding-top: 10px;">Perfil</span></a></li>
+            <li><a href="#" class="menu-item"><img style="border-radius: 15px; " src="../Ícones/Criar Ordem.svg"><span style="font-family: montserrat; font-size: 15px; padding-top: 10px;">Ordens</span></a></li>
+            <li><a href="#" class="menu-item"><img style="border-radius: 15px; " src="../Ícones/Mercado.svg"><span style="font-family: montserrat; font-size: 15px; padding-top: 10px;">ABC</span></a></li>
+            <li><a href="#" class="menu-item"><img style="border-radius: 15px; " src="../Ícones/Conta.svg"><span style="font-family: montserrat; font-size: 15px; padding-top: 10px;">outros</span></a></li>
+        </ul>
+    </div>
+    <div class="caixadomercado">
+        <div class="mercadocima">
+            <h3 style="font-family: montserrat;">Você deseja</h3>
+            <button style="margin-left: 1vh; border-radius: 15px;" class="button">Comprar</button>
+            <h3 style="font-family: montserrat; margin-left: 1vw; margin-right: 1vw;">ou</h3>
+            <button style="background-color: #ff4e44; margin-left: 1vh; border-radius: 15px;" class="button">Vender</button>
+                    <h3 style="font-family: montserrat; margin-left: 1vw;">?</h3>
+        </div>
+        <div class="mercadobaixo">
+            <div class="colunas">
+                <h3 class="h3coluna">Nome público</h3>
+                <label class="colunalabel" id="nome_publico">MarcosSSB41</label>
+            </div>
+            <div class="colunas">
+                <h3 class="h3coluna">Tipo da Ordem</h3>
+                <label class="colunalabel" id="ordem">Venda</label>
+            </div>
+            <div class="colunas">
+                <h3 class="h3coluna">Quantia</h3>
+                <label class="colunalabel" id="quantia">300 - 800 R$</label>
+            </div>
+            <div class="colunas">
+                <h3 class="h3coluna">Tempo de expiração</h3>
+                <label style="color: black; background-color: #FFD700;" class="colunalabel" id="tempo_restante">11H49m</label>
+            </div>
+            <div class="colunas">
+                <h3 class="h3coluna">Sats</h3>
+                <label class="colunalabel" id="sats">90.000</label>
+            </div>
+            <div class="colunas">
+                <h3 class="h3coluna">Bônus/Ônus</h3>
+                <label style="background-color: #ff4e44;" class="colunalabel" id="bonus">- 20% Ônus</label>
+            </div>
+            <div class="colunas">
+                <h3 class="h3coluna">Garantia</h3>
+                <label class="colunalabel" id="garantia">3%</label>
+            </div>
+        </div>
+        <div class="mercadobaixo">
+            <div class="colunas">
+                <h3 class="h3coluna">Nome público</h3>
+                <label class="colunalabel" id="nome_publico">SafelyArmed</label>
+            </div>
+            <div class="colunas">
+                <h3 class="h3coluna">Tipo da Ordem</h3>
+                <label class="colunalabel" id="ordem">Compra</label>
+            </div>
+            <div class="colunas">
+                <h3 class="h3coluna">Quantia</h3>
+                <label class="colunalabel" id="quantia">150 - 200 R$</label>
+            </div>
+            <div class="colunas">
+                <h3 class="h3coluna">Tempo de expiração</h3>
+                <label style="color: black; background-color: #FFD700;" class="colunalabel" id="tempo_restante">12H23m</label>
+            </div>
+            <div class="colunas">
+                <h3 class="h3coluna">Sats</h3>
+                <label class="colunalabel" id="sats">23.000</label>
+            </div>
+            <div class="colunas">
+                <h3 class="h3coluna">Bônus/Ônus</h3>
+                <label style="background-color: #34C848;" class="colunalabel" id="bonus">15% Bônus</label>
+            </div>
+            <div class="colunas">
+                <h3 class="h3coluna">Garantia</h3>
+                <label class="colunalabel" id="garantia">3%</label>
+            </div>
+</div>
+    <script>
+        const menuItems = document.querySelectorAll('.menu-item');
+    
+    menuItems.forEach(item => {
+        item.addEventListener('click', () => {
+    
+            menuItems.forEach(i => i.classList.remove('active'));
+    
+            item.classList.add('active');
+        });
+    });
+    const dropdownButton = document.querySelector('.dropdown button');
+    const dropdownItems = document.querySelectorAll('.dropdown-item');
+    </script>
+</body>
 </html>
