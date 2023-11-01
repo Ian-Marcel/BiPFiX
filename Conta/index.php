0909fcc62f90c050c0b1bb447724152913c7e34d
XoXo <luv@yourxo.xo>
2023-10-31 22:08:01 -0300

    fav added

--- Conta/index.php
+++ Conta/index.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link rel="shortcut icon" href="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbrandslogos.com%2Fwp-content%2Fuploads%2Fimages%2Flarge%2Fphp-logo.png&f=1&nofb=1&ipt=41acf791031a9c7f7571982d2ab60ad04951397125ce67492e5feb31bc878e13&ipo=images" type="image/x-icon">
+        <link rel="shortcut icon" href="../Ícones/Fav.ico" type="image/x-icon">
         <title>BiPFiX - Conta</title>
     </head>
     <body>
