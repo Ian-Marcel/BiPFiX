517054aba77a53456b98fe3323387baa479a86d1
victorhgmoraes <victormoraesgit@gmail.com>
2023-11-23 14:22:04 -0300

    ...

--- LogIn/cadastro.php
+++ LogIn/cadastro.php
@@ -4,34 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BiPFiX - Cadastro</title>
-    <link rel="shortcut icon" href="../Ícones/Fav.ico" type="image/x-icon">
+    <title>Cadastro</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="cadastro.css">
 </head>
 <body>
     <div class="page">
         <form method="POST" class="formCadastro" action="cadastro.php">
-        <h1>Cadastro</h1>
-        <p>Preencha os campos abaixo para criar uma conta.</p>
+            <h1>Cadastro</h1>
+            <p>Preencha os campos abaixo para criar uma conta.</p>
             <label for="username"><i class="fa-solid fa-user"></i> Nome de Identificação</label>
             <div class="container-input1"></div>
-                <input class="input-field" type="text" name="username" placeholder="Escreva seu nome de Identificação" required autofocus="true"/>
+                <input class="input-field" type="text" name="id_name" placeholder="Escreva seu nome de Identificação" required autofocus="true"/>
             <label for="username"><i class="fa-solid fa-user"></i> Nome de Exibição</label>
             <div class="container-input2"></div>
-                <input class="input-field" type="text" name="displayname" placeholder="Digite seu nome de exibição" required/>
+                <input class="input-field" type="text" name="pub_name" placeholder="Digite seu nome de exibição" required/>
             <label for="password"><i class="fa-solid fa-lock"></i> Senha</label>
             <div class="container-input6">
                 <div class="show-password-toggle">
-                    <input class="input-field" type="password" name="password" placeholder="Digite sua senha" required/>
-                    <i class="show-password-icon far fa-eye-slash"></i>
+                    <input id="password" class="input-field" type="password" name="passwd" placeholder="Digite sua senha" required/>
+                    <i id="togglePassword" class="show-password-icon far fa-eye-slash"></i>
                 </div>    
             </div>
             <label for="password"><i class="fa-solid fa-lock"></i> Confirme a senha</label>
             <div class="container-input6">
                 <div class="show-password-toggle">
-                    <input class="input-field" type="password" name="confirmpassword" placeholder="Repita a senha" required/>
-                    <i class="show-password-icon far fa-eye-slash"></i>
+                    <input id="confirmPassword" class="input-field" type="password" name="confirmpassword" placeholder="Repita a senha" required/>
+                    <i id="toggleConfirmPassword" class="show-password-icon far fa-eye-slash"></i>
                 </div>    
             </div>
             <a href="../LogIn/login.php">Já tem uma conta? Faça login</a>
@@ -39,12 +38,28 @@
         </form>
     </div>
     <script>
-        const passwordInput = document.querySelector('input[type="password"]');
-        const showPasswordIcon = document.querySelector('.show-password-icon');
+        const togglePassword = document.getElementById('togglePassword');
+        const passwordInput = document.getElementById('password');
 
-        showPasswordIcon.addEventListener('click', function() {
+        togglePassword.addEventListener('click', function() {
             const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
             passwordInput.setAttribute('type', type);
+
+            if (type === 'text') {
+                this.classList.remove('fa-eye-slash');
+                this.classList.add('fa-eye');
+            } else {
+                this.classList.remove('fa-eye');
+                this.classList.add('fa-eye-slash');
+            }
+        });
+
+        const toggleConfirmPassword = document.getElementById('toggleConfirmPassword');
+        const confirmPasswordInput = document.getElementById('confirmPassword');
+
+        toggleConfirmPassword.addEventListener('click', function() {
+            const type = confirmPasswordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+            confirmPasswordInput.setAttribute('type', type);
 
             if (type === 'text') {
                 this.classList.remove('fa-eye-slash');
@@ -58,54 +73,53 @@
 </body>
 </html>
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $displayname = $_POST["displayname"];
-    $password = $_POST["password"];
+    $id_name = $_POST["id_name"];
+    $pub_name = $_POST["pub_name"];
+    $passwd = $_POST["passwd"];
     $confirmpassword = $_POST["confirmpassword"];
 
-    // Verifique se as senhas coincidem
-    if ($password == $confirmpassword) {
+    if ($passwd == $confirmpassword) {
         try {
-            // Conecte-se ao banco de dados usando o PDO
-            $host = "localhost"; // Host do banco de dados
-            $dbname = "bipfix"; // Nome do banco de dados
-            $user = "root"; // Nome de usuário do banco de dados
-            $pass = ""; // Senha do banco de dados
+            
+            $host = "localhost"; 
+            $dbname = "bipfix"; 
+            $user = "root";
+            $pass = "";
 
             $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 
-            // Verifique se o nome de usuário já existe
-            $check_sql = "SELECT * FROM usuarios WHERE username = :username";
+            $check_sql = "SELECT * FROM account WHERE id_name = :id_name";
             $check_stmt = $pdo->prepare($check_sql);
-            $check_stmt->bindParam(':username', $username);
+            $check_stmt->bindParam(':id_name', $id_name);
             $check_stmt->execute();
 
             if ($check_stmt->rowCount() > 0) {
                 echo "Nome de usuário já está em uso. Escolha outro nome.";
             } else {
                 // O nome de usuário é único, então insira o novo usuário no banco de dados
-                $insert_sql = "INSERT INTO usuarios (username, displayname, password) VALUES (:username, :displayname, :password)";
+                $insert_sql = "INSERT INTO account (id_name, pub_name, passwd) VALUES (:id_name, :pub_name, :passwd)";
                 $insert_stmt = $pdo->prepare($insert_sql);
-                $insert_stmt->bindParam(':username', $username);
-                $insert_stmt->bindParam(':displayname', $displayname);
-                $insert_stmt->bindParam(':password', $password);
+                $insert_stmt->bindParam(':id_name', $id_name);
+                $insert_stmt->bindParam(':pub_name', $pub_name);
+                $insert_stmt->bindParam(':passwd', $passwd);
                 $insert_stmt->execute();
 
                 echo "Cadastro realizado com sucesso!";
 
-                header("Location: cadastro.php"); // Substitua "confirmacao.php" pelo nome da sua página de confirmação
+                header("Location: cadastro.php"); // Substitua "cadastro.php" pelo nome da sua página de confirmação
                 exit; // Encerre o script após o redirecionamento
             }
         } catch (PDOException $e) {
             echo "Erro ao cadastrar: " . $e->getMessage();
+            var_dump($e); // Exibe informações detalhadas do erro
         }
+        
     } else {
         echo "As senhas não coincidem.";
     }
 }
-
 ?>
-
-
-    
