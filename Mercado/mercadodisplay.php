1c671dfa6a7b660efa52d964b85fe75145121121
MatheusGoncalvesM <matheusmachado2005@hotmail.com>
2023-12-03 17:54:25 -0300

    Mercado, satoshis para real

--- Mercado/mercadodisplay.php
+++ Mercado/mercadodisplay.php
@@ -13,49 +13,53 @@ try {
 }
 
 function displayOrders($orderType, $currentPage) {
-    global $pdo; // Adiciona esta linha para tornar $pdo global dentro da função
+    global $pdo;
 
     try {
-        // Exibir as ordens em duas colunas de quatro blocos
-        $numBlocks = 4; // Número de blocos por coluna
-
-        // Consulta para obter as ordens do banco de dados
+        $numBlocks = 4; 
         $sql = "SELECT * FROM orders WHERE status = 'created' AND type = :type LIMIT :limit OFFSET :offset";
         $stmt = $pdo->prepare($sql);
         $stmt->bindValue(':type', $orderType, PDO::PARAM_STR);
-        $stmt->bindValue(':limit', $numBlocks * 2, PDO::PARAM_INT); // Exibe o dobro para lidar com duas colunas
+        $stmt->bindValue(':limit', $numBlocks * 2, PDO::PARAM_INT);
         $stmt->bindValue(':offset', $currentPage * $numBlocks * 2, PDO::PARAM_INT);
         $stmt->execute();
         $orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        // Abre a div de status-markets fora dos loops
         echo "<div class='status-markets'>";
 
-        for ($j = 0; $j < 2; $j++) { // Duas colunas
+        for ($j = 0; $j < 2; $j++) { 
             echo "<div class='market-row'>";
 
             for ($i = $j * $numBlocks; $i < ($j + 1) * $numBlocks; $i++) {
                 echo "<div class='market-block'>";
 
                 if ($i < count($orders)) {
-                    echo "<h3>Sats: {$orders[$i]['v_btc']}</h3>";
-                    echo "<h3>Valor em R$: {$orders[$i]['v_brl']}</h3>";
+                    $valorBRL = $orders[$i]['v_brl'];
+                    $valorBTC = converterReaisParaSatoshis($valorBRL);
+
+                    echo "<h3>Sats: {$valorBTC}</h3>";
+                    echo "<h3>Valor em R$: {$valorBRL}</h3>";
                     echo "<h3>Bônus/Ônus: {$orders[$i]['percentage']}%</h3>";
                 } else {
-                    // Adicione uma classe para estilizar visualmente os blocos vazios
                     echo "<div class='empty-block'></div>";
                 }
 
                 echo "</div>";
             }
 
-            echo "</div>"; // Fecha a div de market-row para cada coluna
+            echo "</div>"; 
         }
-
-        // Fecha a div de status-markets fora dos loops
-        echo "</div>";
-
+        
     } catch (PDOException $e) {
         echo "Erro ao obter as ordens do banco de dados: " . $e->getMessage();
     }
 }
+
+function converterReaisParaSatoshis($valorBRL) {
+    $taxaDeConversao = 4; 
+
+    $valorSatoshis = $valorBRL * 1e3 / $taxaDeConversao;
+
+    return $valorSatoshis;
+}
+?>
