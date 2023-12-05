9687b796fc56598701b5838849e31131cfe9b2b4
XoXo <luv@yourxo.xo>
2023-12-05 08:12:50 -0300

    final

---
+++ Mercado/transaction/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BiPFiX - Transação</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../Design/Fonts/Satoshi/css/satoshi.css">
+    <link rel="stylesheet" href="../Design/Fonts/Montserrat/Montserrat.css">
+    <link rel="shortcut icon" href="../Design/Icons/Favicon.ico" type="image/x-icon">
+</head>
+<body style="background: #000000;">
+    <!-- DOCK MENU -->
+    <header id="dock">
+        <nav class="dock">
+            <ul>
+                <li>
+                    <a href="../../Mais/" class="APP" title="Mais">
+                        <img src="../../Design/Icons/Dock/Mais.png" alt="MaisIMG">
+                    </a>
+                </li>
+                <li>
+                    <a href="../../Suas Ordens/" class="APP" title="Suas Ordens">
+                        <img src="../../Design/Icons/Dock/Suas ordens.png" alt="Suas_OrdensIMG">
+                    </a>
+                </li>
+                <li>
+                    <a href="../../Criar Ordem/" class="APP" title="Criar Ordem">
+                        <img src="../../Design/Icons/Dock/Criar ordem.png" alt="Criar_OrdemIMG">
+                    </a>
+                </li>
+                <li>
+                    <a href="../../Mercado/" class="APP" title="Mercado">
+                        <img src="../../Design/Icons/Dock/Mercado.png" alt="MercadoIMG">
+                    </a>
+                </li>
+                <li>
+                    <a href="../../Conta/" class="APP" title="Conta">
+                        <img src="../../Design/Icons/Dock/Conta.png" alt="ContaIMG">
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- TRANSAÇÃO CONTENT -->
+    <h1 class="uni">Quer mesmo começar essa transação?</h1>
+    <main>
+            <div class="users">
+                <section class="maker">
+                    <p>Maker01193</p>
+                    <span>Está como: vendedor</span>
+                    <h4>Offline</h4>
+                </section>
+                <section class="taker">
+                    <p>Você</p>
+                    <span>Está como: comprador</span>
+                    <h4>Online</h4>
+                </section>
+            </div>
+            <hr>
+            <div class="more_info">
+                <p>Você estará comprando 216841 Satoshis</p>
+                <p>Por R$ 400</p>
+                <p>A 3% do mercado</p>
+            </div>
+            <hr>
+            <div class="confirm">
+                <a href="step1.html">Confirmar negociação?</a>
+                <br><br>
+                <section>Atenção! Você só poderá cancelar esta negociação junto do consentimento da outra parte!</section>
+            </div>
+    </main>
+</body>
+</html>
